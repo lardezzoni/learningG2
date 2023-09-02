9a7a806f29329f991d591101e41c520e4fa0730d
Luiz Henrique Ardezzoni <143741357+lardezzoni@users.noreply.github.com>
2023-09-02 09:14:43 -0300

    Create script.js

---
+++ IntroductionHTMLCSSJS/script.js
@@ -0,0 +1,24 @@
+function write(){
+     var t1 = "test write";
+     document.write(t1);
+
+      }
+
+function aler(){
+    alert("script running");
+}
+
+     convertCelsius(){
+        var b1 = getElementById("c");
+        b1 = b1 + 10000;
+
+        return document.getElementById("msg").innerHTML = b1
+     } 
+
+     class test{
+    
+    constructor(){
+        alert("test alert");
+        
+     }
+     
